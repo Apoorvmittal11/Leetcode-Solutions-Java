fcd3f750a22c4efed213e4ab3606da4332111092
Apoorvmittal11 <149801924+Apoorvmittal11@users.noreply.github.com>
2024-09-17 07:57:27 +0530

    [605] [Time Beats: 97.85%] [Memory Beats: 77.45%] - LeetPush

---
+++ 605-Can-Place-Flowers.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if (n == 0) {
+            return true;
+        }
+        int ans = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0)
+                    && (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
+                flowerbed[i] = 1;
+                ans++;
+            }
+            if (ans == n) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
