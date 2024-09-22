5c946b463439a0eb0b222ab4d41cfafb2bf9f5e6
Apoorvmittal11 <149801924+Apoorvmittal11@users.noreply.github.com>
2024-09-22 10:24:49 +0530

    [440] [Time Beats: 100.00%] [Memory Beats: 28.68%] - LeetPush

---
+++ 440-K-th-Smallest-in-Lexicographical-Order.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int findKthNumber(int n, int k) {
+        int curr = 1;
+        k--;
+        while (k > 0) {
+            int steps = findSteps(n, curr, curr + 1);
+            if (steps <= k) {
+                curr++;
+                k -= steps;
+            } else {
+                curr *= 10;
+                k--;
+            }
+        }
+
+        return curr;
+    }
+
+    private int findSteps(int n, long prefix, long nextPrefix) {
+        int steps = 0;
+        while (prefix <= n) {
+            steps += Math.min(n + 1, nextPrefix) - prefix;
+            prefix *= 10;
+            nextPrefix *= 10;
+        }
+        return steps;
+    }
+}
