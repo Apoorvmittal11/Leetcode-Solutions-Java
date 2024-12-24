e4bb64daf65f9f84e78049ae9207b9862033b422
Apoorvmittal11 <149801924+Apoorvmittal11@users.noreply.github.com>
2024-12-24 19:34:13 +0530

    [1552] [Time Beats: 36.95%] [Memory Beats: 6.83%] - LeetPush

---
+++ 1552-Magnetic-Force-Between-Two-Balls.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int maxDistance(int[] position, int m) {
+        Arrays.sort(position);
+        int low = 1;
+        int high = position[position.length - 1] - position[0];
+
+        int ans = 0;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (helper(position, mid, m)) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+
+    }
+
+    public boolean helper(int position[], int x, int count) {
+
+        int prev = position[0];
+        count--;
+        for (int i = 1; i < position.length; i++) {
+            if (position[i] - prev >= x) {
+                count--;
+                if (count == 0) {
+                    return true;
+                }
+                prev = position[i];
+            }
+        }
+
+        return count == 0;
+    }
+}
