533416acfab7259146ec09a7ca912d10562290b3
Apoorvmittal11 <149801924+Apoorvmittal11@users.noreply.github.com>
2024-12-28 17:58:37 +0530

    [559] [Time Beats: 5.11%] [Memory Beats: 29.37%] - LeetPush

---
+++ 559-Maximum-Depth-of-N-ary-Tree.java
@@ -0,0 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public int maxDepth(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<Node> q = new LinkedList<>();
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> res = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                Node node = q.poll();
+                res.add(node.val);
+                if (node.children != null && !node.children.isEmpty()) {
+                    for (Node child : node.children) {
+                        q.add(child);
+                    }
+                }
+            }
+            ans.add(res);
+        }
+        return ans.size();
+    }
+}
