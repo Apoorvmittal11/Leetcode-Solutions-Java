31956861f7515309d86ff28c8a87fb06bdd1ae84
Apoorvmittal11 <149801924+Apoorvmittal11@users.noreply.github.com>
2025-03-27 23:13:01 +0530

    [1344] [Time Beats: 100.00%] [Memory Beats: 62.10%] - LeetPush

---
+++ Angle-Between-Hands-of-a-Clock.java
@@ -0,0 +1,8 @@
+class Solution {
+    public double angleClock(int hour, int minutes) {
+        double res = Math.abs((30 * hour + 0.5 * minutes) - (6 * minutes));
+        if (res > 180)
+            return 360 - res;
+        return res;
+    }
+}
